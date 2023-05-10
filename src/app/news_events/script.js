345cb55559d73591678380be66bd5be4659c819a
ron19102004 <ron19102004@gmail.com>
2023-05-10 19:19:47 +0700

    uo

---
+++ src/app/news_events/script.js
@@ -0,0 +1,78 @@
+const listMenu = [
+   {
+      href: '#',
+      title: 'GIỚI THIỆU'
+   },
+   {
+      href: '#',
+      title: 'TIN TỨC & SỰ KIỆN'
+   },
+   {
+      href: '#',
+      title: 'TUYỂN SINH'
+   },
+   {
+      href: '#',
+      title: 'ĐÀO TẠO'
+   },
+   {
+      href: '#',
+      title: 'SINH VIÊN'
+   },
+   {
+      href: '#',
+      title: 'KHOA HỌC CÔNG NGHỆ'
+   },
+   {
+      href: '#',
+      title: 'LIÊN HỆ'
+   }
+]
+const renderListMenu = (listMenu) => {
+   let src = '<li class="li "> <img src="../../assets/home.png" alt="home-icon" class="w-6"></li>';
+   listMenu.forEach(item => {
+      src += `
+               <li class="li text-white ">
+                     <a href="${item.href}">${item.title}</a>
+               </li>`
+   });
+   return src;
+}
+const refreshFrom = () => {
+   $(() => {
+      $("#name").val("");
+      $("#email").val("");
+      $("#tel").val("");
+      $("#title").val("");
+      $("#content").val("");
+      $("#require").val("null");
+   })
+   alert("Đã thiết lập lại các giá trị ban đầu")
+}
+$(() => {
+   //renderListMenu
+   $("#list-menu").html(renderListMenu(listMenu))
+   $("#list-menu-mobile").html(renderListMenu(listMenu))
+   $("#refreshButton").click((e) => {
+      e.preventDefault();
+      refreshFrom();
+   })
+   $("#menu-icon").click(() => {
+      $("#responsive-mobile-menu").toggle('slow');
+      $("#menu-icon").css({
+         display: 'none'
+      })
+      $("#close-icon").css({
+         display: 'block'
+      })
+   })
+   $("#close-icon").click(() => {
+      $("#responsive-mobile-menu").toggle('slow');
+      $("#close-icon").css({
+         display: 'none'
+      })
+      $("#menu-icon").css({
+         display: 'block'
+      })
+   })
+})
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        while: {
+          color1: '#153E90',
+          color2: '#0E49B5',
+          color3: '#54E346',
+          color4: '#FFFAA4',
+        },
+    },
+  },
+},
+  plugins: [],
+}
